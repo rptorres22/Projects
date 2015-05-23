4ecb5e66c1ed7579451557153a1b4371cd4526c9
Rowell Torres <rtorres22@gmail.com>
2015-05-23 14:27:38 -0700

    added article model

---
+++ meanprojfirst/app/models/article.server.model.js
@@ -0,0 +1,40 @@
+// app/models/article.server.model.js
+
+/*
+	This will represent the article entity
+*/
+
+var mongoose 	= require('mongoose'),
+	Schema 		= mongoose.Schema;
+
+
+
+var ArticleSchema = new Schema({
+
+	created: {
+		type: Date,
+		default: Date.now
+	},
+
+
+	title: {
+		type: String,
+		default: '',
+		trim: true,
+		required: 'Title cannot be blank'
+	},
+
+	
+	content: {
+		type: String,
+		default: '',
+		trim: true
+	},
+
+
+	creator: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+
+})
