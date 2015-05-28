379b0f8e2b0a5590e3d07787a2139d9a0683f3c3
rptorres22 <rtorres22@gmail.com>
2015-05-28 08:18:17 -0700

    added comments for clarity

--- meanprojfirst/config/config.js
+++ meanprojfirst/config/config.js
@@ -7,5 +7,4 @@
 	This will load the appropriate configuration file inside the /config/env/ 
 	folder based on the environment 'NODE_ENV' variable.
 */
-
 module.exports = require('./env/' + process.env.NODE_ENV + '.js');
