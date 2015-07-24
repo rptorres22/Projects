3fc2abec8ecda637692c182f7136b0eb81ced186
rptorres22 <rptorres22@gmail.com>
2015-07-24 10:06:16 -0700

    added login/signup/directives controllers/views

---
+++ showtrackr/public/controllers/signup.js
@@ -0,0 +1,11 @@
+angular.module('MyApp')
+	.controller('SignupCtrl', ['$scope', 'Auth', function($scope, Auth) {
+		
+		$scope.signup = function() {
+			Auth.signup({
+				email: $scope.email,
+				password: $scope.password
+			});
+		};
+		
+	}]);
